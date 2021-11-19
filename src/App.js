cb6b9c03fe937e97c0e93f540a44beaa5183b238
Samuel <samuel.soares@unidesk.com.br>
2021-11-19 19:55:34 -0300

    Trabalhando com hooks em react.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+  
+  const [input, setInput] = useState('');
+  
+  //SUBSTITUI COMPONENTE O COMPONENTE DID MOUNT
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  //SUBSTITUI COMPONENTE DID UPDATE 
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  /*
+  function handlerAdd(){
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }*/
+
+  // REACRIADO APENAS QUANDO PRECISAMOS UTILIZAR
+  const handlerAdd = useCallback(() => {
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }, [tarefas, input]);
+
+  // RETORNA CALCULO QUANDO EU QUISER
+  const totalTarefas = useMemo(() => tarefas.length, [tarefas]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul><br />
+      <strong> Você tem { totalTarefas } tarefas!</strong><br/>
+      <input type="text" value={input}
+        onChange={(e) => setInput(e.target.value)} />
+     <button type="button" onClick={handlerAdd}>Adicionar</button>
     </div>
   );
 }
